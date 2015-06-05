303cac7c4ebb0a2664b0a1ca094afe8c4c5521a3
aayush382 <aayush382@gmail.com>
2015-06-05 07:12:04 -0400

    adding the linked list implementation

--- myQueue.cpp
+++ myQueue.cpp
@@ -37,3 +37,60 @@ class myqueue
  vector<int> _table;
  int _tail;
 };
+
+
+//Linked list representation
+
+class node {
+    public:
+    int val;
+    node *next;
+    node(int val) : val(val), next(NULL)  {}
+};
+
+class myqueue
+{
+    //push from tail, pop from head
+ public:    
+ myqueue():tail(NULL), head(NULL) {}
+ void push(int elem)
+ {
+     
+     node * curr= new node(elem);
+     if(!head)
+        { //This is important and mistakes occurr here: you need to intialize head and tail when queuue has been emptied or for the first time
+        head=curr;
+        tail=curr;
+        }
+    else
+        {
+        tail->next=curr;
+        tail=curr;
+        }
+     
+ }
+ void pop()
+ {
+     if(!head)
+     {
+        cout << " Error can't pop empty queue" << endl;
+        return;
+     }    
+     head=head->next;
+     
+ }
+ bool isEmpty()
+ {
+     return head==NULL;
+ }
+ int front()
+ {
+     if(head)
+        return head->val;
+     else 
+        return -1;
+ }
+ 
+ private:
+ node * head, *tail;
+};
